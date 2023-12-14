ae699e6ca926391bb21f36ca1b18193c11f6fe86
админ <lodbrokragnar548@gmail.com>
2023-12-14 20:52:24 +0300

    pp3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println("..................................");
+
+        Cat sheril =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(sheril.getMessage());
+        Cat sharikat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(sharikat.getMessage());
+
+        System.out.println(bean == bean1);
+        System.out.println(sheril == sharikat);
     }
 }
